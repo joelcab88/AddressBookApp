748d15963b3ede77fd94722046d3901ac709d15b
Joel Cab Yah <joelcab88@gmail.com>
2022-07-26 09:44:51 -0500

    JFCABY : Se implementa interface en servicio api.

---
+++ Service/ContactServices.cs
@@ -0,0 +1,27 @@
+﻿using AddressBookApp.DTO;
+using AddressBookApp.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookApp.Service
+{
+    public class ContactServices : IContactServices
+    {
+        public bool DeleteContact(int iIdContacto_)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContactDTO GetAllContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContactDTO GetContactById(int iIdContacto_)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
