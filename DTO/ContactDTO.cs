f607408c6a5870a471b0cbde5418fd1aa72a2566
Joel Cab Yah <joelcab88@gmail.com>
2022-07-26 00:44:41 -0500

    JFCABY : Se agrega dto para deserializar json data.

---
+++ DTO/ContactDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AddressBookApp.DTO
+{
+    public class ContactDTO
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public List<string> addressLines { get; set; }
+    }
+}
