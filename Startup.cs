24755991b2b00a8418b90068d44aef3a8bbb2359
Joel Cab Yah <joelcab88@gmail.com>
2022-07-26 10:30:21 -0500

    JFCABY : Se agrega swagger.

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,28 @@ namespace AddressBookApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            AddSwagger(services);
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"API TEST {groupName}",
+                    Version = groupName,
+                    Description = "API TEST",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "PRACTICAL TEST",
+                        Email = string.Empty,
+                        Url = new Uri("https://localhost:44348/swagger/index.html"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +68,12 @@ namespace AddressBookApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foo API V1");
             });
         }
     }
